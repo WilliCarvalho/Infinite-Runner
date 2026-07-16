00a1d50d72ee2f140dd38861b659172cead87713
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2026-07-16 16:19:07 -0300

    Obstacle implemented
    - Obstacle animated
    - Obstacle move all the screen in -X direction, in loop
    - Obstacle has NO Coliision

--- src/main.cpp
+++ src/main.cpp
@@ -18,16 +18,32 @@ int main()
 
 	SearchAndSetResourceDir("resources");
 
-
-	//Resizing Texture because it was too small
-	Image img = LoadImage("2DTextures/Character/CharacterSheet.png");
+	//Resizing Obstacle Texture because it was too small
+	Image img = LoadImage("2DTextures/Enemy/EnemySheet.png");
 	int newWidth = static_cast<int>(img.width * 4);
 	int newHeight = static_cast<int>(img.height * 4);
 	ImageResizeNN(&img, newWidth, newHeight);
 
+	//Obstacle Data
+	Texture2D obstacleTexture = LoadTextureFromImage(img);
+	Rectangle obstacleRec{ 0.f,obstacleTexture.height / 3, obstacleTexture.width, obstacleTexture.height / 3 };
+	Vector2 obstaclePos{ windowWidth, windowHeight - obstacleRec.height };
+	int obstacleVelocity = -400;
+	int obstacleCurrentFrame = 1;
+	const float obstacleUpdateFrameTime = 1.f / 6.f;
+	float obstacleAnimRunningTime = 0.f;
+
+
+
+	//Resizing Player Texture because it was too small
+	Image img2 = LoadImage("2DTextures/Character/CharacterSheet.png");
+	int newWidth2 = static_cast<int>(img2.width * 4);
+	int newHeight2 = static_cast<int>(img2.height * 4);
+	ImageResizeNN(&img2, newWidth2, newHeight2);
+
 	//PLayer Data
-	Texture2D PlayerTexture = LoadTextureFromImage(img);
-	UnloadImage(img);
+	Texture2D PlayerTexture = LoadTextureFromImage(img2);
+	UnloadImage(img2);
 
 	Rectangle playerRectangle;
 	playerRectangle.width = PlayerTexture.width / 4;
@@ -39,14 +55,15 @@ int main()
 	Vector2 playerPos;
 	playerPos.x = windowWidth / 2 - playerRectangle.width / 2;
 	playerPos.y = windowHeight - playerRectangle.height;
-
-	//player animation frame data
-	int currentFrame = 0;
-	const float UpdateFrameTime = 1.f / 12.f; //Amount of time before update the animation
-
 	int jumpForce = -600; //in pixels/s
 	int yVelocity = 0;
 	bool isInAir = false;
+
+	//player animation frame data
+	int playerCurrentFrame = 0;
+	const float updateFrameTime = 1.f / 12.f; //Amount of time before update the animation (bigger numbers make animation faster)
+	float playerAnimRunningTime = 0;
+
 
 	//LOGS
 	//TraceLog(LOG_INFO, "PLAYER RECTANGLE WIDTH: %f", playerRectangle.width);
@@ -56,14 +73,12 @@ int main()
 	int floorPos = windowHeight - playerRectangle.height;
 	const int gravityVelocity = 1'500; //acceleration due to gravity (pixels/s)/s
 	float deltaTime = 0;
-	float runningTime = 0;
 
 
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
 		deltaTime = GetFrameTime();
-		runningTime += deltaTime;
 		BeginDrawing();
 
 		ClearBackground(GREEN);
@@ -89,13 +104,21 @@ int main()
 
 		playerPos.y += yVelocity * deltaTime;
 
-		//Update animation frame
-		if (runningTime >= UpdateFrameTime)
+		obstaclePos.x += obstacleVelocity * deltaTime;
+
+		if (obstaclePos.x < -obstacleRec.width)
 		{
-			runningTime = 0.f;
-			playerRectangle.x = currentFrame * playerRectangle.width;
+			obstaclePos.x = windowWidth;
+		}
+
+		//Update  player animation frame
+		playerAnimRunningTime += deltaTime;
+		if (playerAnimRunningTime >= updateFrameTime && isInAir == false)
+		{
+			playerAnimRunningTime = 0.f;
+			playerRectangle.x = playerCurrentFrame * playerRectangle.width;
 			//Adjusting frame position Y based on currentFrame (the first frame of walking animation is alone on it's row
-			if (currentFrame >= 1)
+			if (playerCurrentFrame >= 1)
 			{
 				playerRectangle.y = PlayerTexture.height / 6;
 			}
@@ -103,18 +126,38 @@ int main()
 			{
 				playerRectangle.y = 0;
 			}
-			currentFrame++;
-			if (currentFrame > 4)
+			playerCurrentFrame++;
+			if (playerCurrentFrame > 4)
 			{
-				currentFrame = 0;
+				playerCurrentFrame = 0;
 			}
 		}
 
+		//Update Obstacle animation frame
+		obstacleAnimRunningTime += deltaTime;
+		if (obstacleAnimRunningTime >= obstacleUpdateFrameTime)
+		{
+			obstacleAnimRunningTime = 0.f;
+			obstacleRec.y = obstacleCurrentFrame * obstacleRec.height;
+
+			obstacleCurrentFrame++;
+
+			if (obstacleCurrentFrame >= 3)
+			{
+				obstacleCurrentFrame = 1;
+			}
+		}
+
+		//DrawPlayer
 		DrawTextureRec(PlayerTexture, playerRectangle, playerPos, WHITE);
+
+		//DrawObstacle
+		DrawTextureRec(obstacleTexture, obstacleRec, obstaclePos, WHITE);
 
 		EndDrawing();
 	}
 	UnloadTexture(PlayerTexture);
+	UnloadTexture(obstacleTexture);
 	CloseWindow();
 	return 0;
 }
