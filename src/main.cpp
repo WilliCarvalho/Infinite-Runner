b34b88ec2bf4243919417a0d71a59ef2510b75ec
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2026-07-16 15:02:50 -0300

    Animated Player

--- src/main.cpp
+++ src/main.cpp
@@ -21,37 +21,49 @@ int main()
 
 	//Resizing Texture because it was too small
 	Image img = LoadImage("2DTextures/Character/CharacterSheet.png");
-	int newWidth = static_cast<int>(img.width * 3);
-	int newHeight = static_cast<int>(img.height * 3);
+	int newWidth = static_cast<int>(img.width * 4);
+	int newHeight = static_cast<int>(img.height * 4);
 	ImageResizeNN(&img, newWidth, newHeight);
 
 	//PLayer Data
 	Texture2D PlayerTexture = LoadTextureFromImage(img);
 	UnloadImage(img);
-	
+
 	Rectangle playerRectangle;
 	playerRectangle.width = PlayerTexture.width / 4;
 	playerRectangle.height = PlayerTexture.height / 6;
+	//Rectangle position inside the spriteSheet (useful for animations)
 	playerRectangle.x = 0;
 	playerRectangle.y = 0;
+
 	Vector2 playerPos;
-	playerPos.x = windowWidth/2 - playerRectangle.width/2;
+	playerPos.x = windowWidth / 2 - playerRectangle.width / 2;
 	playerPos.y = windowHeight - playerRectangle.height;
-	int jumpForce = -20;
+
+	//player animation frame data
+	int currentFrame = 0;
+	const float UpdateFrameTime = 1.f / 12.f; //Amount of time before update the animation
+
+	int jumpForce = -600; //in pixels/s
 	int yVelocity = 0;
 	bool isInAir = false;
 
-	TraceLog(LOG_INFO, "PLAYER RECTANGLE WIDTH: %f", playerRectangle.width);
-	TraceLog(LOG_INFO, "Player rectangle height: %f", playerRectangle.height);
+	//LOGS
+	//TraceLog(LOG_INFO, "PLAYER RECTANGLE WIDTH: %f", playerRectangle.width);
+	//TraceLog(LOG_INFO, "Player rectangle height: %f", playerRectangle.height);
 
 	//Environment Data
 	int floorPos = windowHeight - playerRectangle.height;
-	const int gravityVelocity = 1;
+	const int gravityVelocity = 1'500; //acceleration due to gravity (pixels/s)/s
+	float deltaTime = 0;
+	float runningTime = 0;
 
 
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
+		deltaTime = GetFrameTime();
+		runningTime += deltaTime;
 		BeginDrawing();
 
 		ClearBackground(GREEN);
@@ -66,7 +78,7 @@ int main()
 		else
 		{
 			//Is in the air
-			yVelocity += gravityVelocity;
+			yVelocity += gravityVelocity * deltaTime;
 			isInAir = true;
 		}
 
@@ -75,7 +87,28 @@ int main()
 			yVelocity += jumpForce;
 		}
 
-		playerPos.y += yVelocity;
+		playerPos.y += yVelocity * deltaTime;
+
+		//Update animation frame
+		if (runningTime >= UpdateFrameTime)
+		{
+			runningTime = 0.f;
+			playerRectangle.x = currentFrame * playerRectangle.width;
+			//Adjusting frame position Y based on currentFrame (the first frame of walking animation is alone on it's row
+			if (currentFrame >= 1)
+			{
+				playerRectangle.y = PlayerTexture.height / 6;
+			}
+			else
+			{
+				playerRectangle.y = 0;
+			}
+			currentFrame++;
+			if (currentFrame > 4)
+			{
+				currentFrame = 0;
+			}
+		}
 
 		DrawTextureRec(PlayerTexture, playerRectangle, playerPos, WHITE);
 
